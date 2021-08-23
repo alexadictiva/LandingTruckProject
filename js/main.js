f48af87638a6131f5a425fe03ed5a8f31d822545
Libia Perez <alexandra220790@gmail.com>
2021-08-23 19:36:07 -0300

    Styling buttons

--- js/main.js
+++ js/main.js
@@ -11,16 +11,16 @@ var swiper1 = new Swiper(".mySwiper1", {
   },
   //autoplay
   autoplay: {
-    delay: 2500,
+    delay: 25000,
     disableOnInteraction: false,
   },
 });
 
 /*FAQs Acordeon JQuery*/
-$('.faqsContainer').on('click','button',function(){
-	var t = $(this);
-	var tp = t.next();
-	var p = t.parent().siblings().find('p');
-	tp.slideToggle();
-	p.slideUp();
+$(".faqsContainer").on("click", "button", function () {
+  var t = $(this);
+  var tp = t.next();
+  var p = t.parent().siblings().find("p");
+  tp.slideToggle();
+  p.slideUp();
 });
